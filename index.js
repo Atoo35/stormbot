1f3fffe98ea2debaaeca285fe884f56ae71d49c9
Atharva Deshpande <adrooney322@gmail.com>
2021-04-11 16:43:14 +0530

    colours changed

--- index.js
+++ index.js
@@ -8,27 +8,31 @@ client.on('ready',()=>{
         var flag=false;
         var guild=client.guilds.cache.get(config.guild)
         setInterval(()=>{
-                var validAplhs = ['0','1','2','3','4','5','6','7',
-                '8','9','a','b','c','d','e','f']
-                var randomColour='#'
-                for(let i=0;i<6;i++){
-                        let newAplh=Math.round(Math.random()*validAplhs.length)
-                        if(newAplh === 16)
-                        newAplh=15
-                        randomColour+=validAplhs[newAplh]
-                }                
+                // var validAplhs = ['0','1','2','3','4','5','6','7',
+                // '8','9','a','b','c','d','e','f']
+                // var randomColour='#'
+                // for(let i=0;i<6;i++){
+                //         let newAplh=Math.round(Math.random()*validAplhs.length)
+                //         if(newAplh === 16)
+                //         newAplh=15
+                //         randomColour+=validAplhs[newAplh]
+                // }   
+                var colours = ['#FE000F','#0054FF','#00FD09','#FFF600']          
                 for(var role in config.roles){
                         let guildRole = guild.roles.cache.get(config.roles[role])
-                        guildRole.setColor(randomColour)
+                        let color = Math.round(Math.random()*colours.length)
+                        if(color === 4)
+                                colour=3
+                        guildRole.setColor(colours[color])
                 }
-        },5*1000)
-        setInterval(async ()=>{
-                var nicks=["SVGムParZivaL","test"]
-                // var nicks=["★𝙋Δ𝙉𝙕Ξ𝙍★","ĐV么666-IRØN"]
-                var guildMember = await guild.members.fetch(config.member)
-                flag?guildMember.setNickname(nicks[0]):guildMember.setNickname(nicks[1]);
-                flag=!flag;
-        },2*1000)
+        },1*1000)
+        // setInterval(async ()=>{
+        //         var nicks=["SVGムParZivaL","test"]
+        //         // var nicks=["★𝙋Δ𝙉𝙕Ξ𝙍★","ĐV么666-IRØN"]
+        //         var guildMember = await guild.members.fetch(config.member)
+        //         flag?guildMember.setNickname(nicks[0]):guildMember.setNickname(nicks[1]);
+        //         flag=!flag;
+        // },2*1000)
 })
 
 
