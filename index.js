40e4f56eee9ef3d1d11c31e61eb649e3a5324448
Atharva Deshpande <adrooney322@gmail.com>
2021-04-11 16:34:12 +0530

    change nickname

--- index.js
+++ index.js
@@ -5,22 +5,30 @@ const config = require('./config.json')
 
 client.on('ready',()=>{
         console.log('Bot is online');
+        var flag=false;
+        var guild=client.guilds.cache.get(config.guild)
         setInterval(()=>{
                 var validAplhs = ['0','1','2','3','4','5','6','7',
-                                  '8','9','a','b','c','d','e','f']
+                '8','9','a','b','c','d','e','f']
                 var randomColour='#'
                 for(let i=0;i<6;i++){
                         let newAplh=Math.round(Math.random()*validAplhs.length)
                         if(newAplh === 16)
-                                newAplh=15
+                        newAplh=15
                         randomColour+=validAplhs[newAplh]
                 }                
-                var guild=client.guilds.cache.get(config.guild)
                 for(var role in config.roles){
                         let guildRole = guild.roles.cache.get(config.roles[role])
                         guildRole.setColor(randomColour)
                 }
         },5*1000)
+        setInterval(async ()=>{
+                var nicks=["SVGムParZivaL","test"]
+                // var nicks=["★𝙋Δ𝙉𝙕Ξ𝙍★","ĐV么666-IRØN"]
+                var guildMember = await guild.members.fetch(config.member)
+                flag?guildMember.setNickname(nicks[0]):guildMember.setNickname(nicks[1]);
+                flag=!flag;
+        },2*1000)
 })
 
 
